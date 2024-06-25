c0a7fc2bff105eb699957fa334b3471fd386e790
MinhBN-dev <bachminh.uit@gmail.com>
2024-06-25 10:56:21 +0700

    Refactor/63/refactor_position: refactor controllers, repositories, schemas, services for positions

--- payroll/positions/controllers.py
+++ payroll/positions/controllers.py
@@ -7,42 +7,54 @@ from payroll.positions.schemas import (
     PositionUpdate,
 )
 from payroll.database.core import DbSession
-from payroll.positions.repositories import (
-    get_all,
-    get_one_by_id,
-    create,
-    update,
-    delete,
+from payroll.positions.services import (
+    create_position,
+    delete_position,
+    get_all_position,
+    get_position_by_id,
+    update_position,
 )
 
 position_router = APIRouter()
 
 
+# GET /positions
 @position_router.get("", response_model=PositionsRead)
 def retrieve_positions(
     *,
     db_session: DbSession,
 ):
-    return get_all(db_session=db_session)
+    """Retrieve all positions."""
+    return get_all_position(db_session=db_session)
 
 
-@position_router.get("/{id}", response_model=PositionRead)
-def retrieve_position(*, db_session: DbSession, id: int):
-    return get_one_by_id(db_session=db_session, id=id)
+# GET /positions/{position_id}
+@position_router.get("/{position_id}", response_model=PositionRead)
+def retrieve_position(*, db_session: DbSession, position_id: int):
+    """Retrieve a position by id."""
+    return get_position_by_id(db_session=db_session, position_id=position_id)
 
 
+# POST /positions
 @position_router.post("", response_model=PositionRead)
-def create_position(*, position_in: PositionCreate, db_session: DbSession):
-    """Creates a new user."""
-    position = create(db_session=db_session, position_in=position_in)
+def create(*, position_in: PositionCreate, db_session: DbSession):
+    """Creates a new position."""
+    position_in.created_by = "admin"
+    position = create_position(db_session=db_session, position_in=position_in)
     return position
 
 
-@position_router.put("/{id}", response_model=PositionRead)
-def update_position(*, db_session: DbSession, id: int, position_in: PositionUpdate):
-    return update(db_session=db_session, id=id, position_in=position_in)
+# PUT /positions/{position_id}
+@position_router.put("/{position_id}", response_model=PositionRead)
+def update(*, db_session: DbSession, position_id: int, position_in: PositionUpdate):
+    """Update a position by id."""
+    return update_position(
+        db_session=db_session, position_id=position_id, position_in=position_in
+    )
 
 
-@position_router.delete("/{id}", response_model=PositionRead)
-def delete_position(*, db_session: DbSession, id: int):
-    return delete(db_session=db_session, id=id)
+# DELETE /positions/{position_id}
+@position_router.delete("/{position_id}")
+def delete(*, db_session: DbSession, position_id: int):
+    """Delete a position by id."""
+    return delete_position(db_session=db_session, position_id=position_id)
