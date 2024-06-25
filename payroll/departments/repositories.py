af5ab10a7423c3347dd65b0a06265d857d6f4a13
MinhBN-dev <bachminh.uit@gmail.com>
2024-06-25 10:47:26 +0700

    Refactor/63/refactor_department: refactor controllers, repositories, schemas, services for departments

--- payroll/departments/repositories.py
+++ payroll/departments/repositories.py
@@ -5,95 +5,72 @@ from payroll.departments.schemas import (
     DepartmentsRead,
     DepartmentUpdate,
 )
-
-from payroll.exception import AppException
-from payroll.exception.error_message import ErrorMessages
 from payroll.models import PayrollDepartment
-from payroll.utils.functions import check_depend_employee
 
 log = logging.getLogger(__name__)
 
 
-def get_department_by_id(*, db_session, id: int) -> PayrollDepartment:
+# GET /departments/{department_id}
+def retrieve_department_by_id(*, db_session, department_id: int) -> PayrollDepartment:
     """Returns a department based on the given id."""
     department = (
-        db_session.query(PayrollDepartment).filter(PayrollDepartment.id == id).first()
-    )
-    return department
-
-
-def check_exist_department_by_id(*, db_session, department_id: int) -> bool:
-    department = get_department_by_id(db_session=db_session, id=department_id)
-    return department is not None
-
-
-def get_department_by_code(*, db_session, code: str) -> PayrollDepartment:
-    """Returns a department based on the given code."""
-    department = (
         db_session.query(PayrollDepartment)
-        .filter(PayrollDepartment.code == code)
+        .filter(PayrollDepartment.id == department_id)
         .first()
     )
     return department
 
 
-def get_all(*, db_session) -> DepartmentsRead:
-    """Returns all departments."""
-    data = db_session.query(PayrollDepartment).all()
-    return DepartmentsRead(data=data)
-
-
-def get_one_by_id(*, db_session, id: int) -> PayrollDepartment:
-    """Returns a department based on the given id."""
-    department = get_department_by_id(db_session=db_session, id=id)
-
-    if not department:
-        raise AppException(ErrorMessages.ResourceNotFound())
+def retrieve_department_by_code(
+    *, db_session, department_code: str
+) -> PayrollDepartment:
+    """Returns a department based on the given code."""
+    department = (
+        db_session.query(PayrollDepartment)
+        .filter(PayrollDepartment.code == department_code)
+        .first()
+    )
     return department
 
 
-def create(*, db_session, department_in: DepartmentCreate) -> PayrollDepartment:
+# GET /departments
+def retrieve_all_departments(*, db_session) -> DepartmentsRead:
+    """Returns all departments."""
+    query = db_session.query(PayrollDepartment)
+    count = query.count()
+    departments = query.all()
+    return {"count": count, "data": departments}
+
+
+# POST /departments
+def add_department(*, db_session, department_in: DepartmentCreate) -> PayrollDepartment:
     """Creates a new department."""
     department = PayrollDepartment(**department_in.model_dump())
-    department_db = get_department_by_code(db_session=db_session, code=department.code)
-    if department_db:
-        raise AppException(ErrorMessages.ResourceAlreadyExists())
     db_session.add(department)
     db_session.commit()
     return department
 
 
-def update(
-    *, db_session, id: int, department_in: DepartmentUpdate
+# PUT /departments/{department_id}
+def modify_department(
+    *, db_session, department_id: int, department_in: DepartmentUpdate
 ) -> PayrollDepartment:
     """Updates a department with the given data."""
-    department_db = get_department_by_id(db_session=db_session, id=id)
-
-    if not department_db:
-        raise AppException(ErrorMessages.ResourceNotFound())
-
-    update_data = department_in.model_dump(exclude_unset=True)
-
-    db_session.query(PayrollDepartment).filter(PayrollDepartment.id == id).update(
-        update_data, synchronize_session=False
+    query = db_session.query(PayrollDepartment).filter(
+        PayrollDepartment.id == department_id
     )
-
+    update_data = department_in.model_dump(exclude_unset=True)
+    query.update(update_data, synchronize_session=False)
     db_session.commit()
-    return department_db
+    updated_department = query.first()
+    return updated_department
 
 
-def delete(*, db_session, id: int) -> PayrollDepartment:
+# DELETE /departments/{department_id}
+def remove_department(*, db_session, department_id: int):
     """Deletes a department based on the given id."""
-    query = db_session.query(PayrollDepartment).filter(PayrollDepartment.id == id)
-    department = query.first()
-
-    if not department:
-        raise AppException(ErrorMessages.ResourceNotFound())
-
-    if check_depend_employee(db_session=db_session, department_id=department.id):
-        raise AppException(ErrorMessages.ExistDependEmployee())
-
-    db_session.query(PayrollDepartment).filter(PayrollDepartment.id == id).delete()
+    db_session.query(PayrollDepartment).filter(
+        PayrollDepartment.id == department_id
+    ).delete()
 
     db_session.commit()
-    return department
