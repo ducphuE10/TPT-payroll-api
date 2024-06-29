b5bd67c966318829ab603611b86830d9565bcd8f
MinhBN-dev <bachminh.uit@gmail.com>
2024-06-29 11:53:17 +0700

    Feat: alembic implementation

--- alembic/env.py
+++ alembic/env.py
@@ -4,16 +4,14 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
 from payroll.models import Base  # noqa
 from payroll.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option(
-    "sqlalchemy.url",
-    f"postgresql+psycopg2://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_SERVER}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}",
-)
+config.set_main_option("sqlalchemy.url", str(settings.SQLALCHEMY_DATABASE_URI))
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 
-python -m payroll.cli database init
+# python -m payroll.cli database init
+poetry run alembic upgrade head
 
 uvicorn payroll.main:app --host 0.0.0.0 --port 8000
