6a090338308fbf3bfbcb637494be8f0133e9daaf
MinhBN-dev <bachminh.uit@gmail.com>
2024-08-30 13:47:29 +0700

    Feature 101: fix payroll management metrics

--- payroll/payroll_managements/controllers.py
+++ payroll/payroll_managements/controllers.py
@@ -32,6 +32,11 @@ def retrieve_payroll_managements(
     )
 
 
+@payroll_management_router.get("/metrics")
+def metrics(*, db_session: DbSession, month: int, year: int):
+    return metrics_handler(db_session=db_session, month=month, year=year)
+
+
 # GET /payroll_managements/{payroll_management_id}
 @payroll_management_router.get(
     "/{payroll_management_id}", response_model=PayrollManagementRead
@@ -41,11 +46,6 @@ def retrieve_payroll_management(*, db_session: DbSession, payroll_management_id:
     return get_payroll_management_by_id(
         db_session=db_session, payroll_management_id=payroll_management_id
     )
-
-
-@payroll_management_router.get("/metrics")
-def metrics(*, db_session: DbSession, month: int, year: int):
-    return metrics_handler(db_session=db_session, month=month, year=year)
 
 
 @payroll_management_router.post("", response_model=PayrollManagementRead)
