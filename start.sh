edbd657ae1bd0eaf446060d1845e1ca7ba0b970a
MinhBN-dev <bachminh.uit@gmail.com>
2024-09-05 14:27:15 +0700

    Refactor: refactor start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
 #! /usr/bin/env bash
 
-# python -m payroll.cli database init
-timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
-
-poetry run alembic revision --autogenerate -m "revision_$timestamp"
-
 poetry run alembic upgrade head
 
 uvicorn payroll.main:app --host 0.0.0.0 --port 8000
